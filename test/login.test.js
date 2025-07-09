680de7f7ae56dc564ae794f5512b25d6c4e342e3
Michele <michelebrunasilva@gmail.com>
2025-07-09 17:41:27 -0300

    Iniciado o projeto

---
+++ test/login.test.js
@@ -0,0 +1,26 @@
+//const { describe } = require("mocha");
+const request = require('supertest');
+const { expect } = require('chai')
+
+//Vai usar o supertest para fazer uma requisição para a API passando credenciais válidas e vamos esperar o status code 200 e que a propriedade token seja String
+
+//este teste é do Mocha
+
+//Quando tem dentro do método que retorna uma promesse, precisa usar o await. E se usar o await, precisa usar o async antes da função anônima
+describe('Login', () => {
+    describe('POST /login', () => {
+        it('Deve retornar 200 com um token em string quando usar credenciais válidas', async () => {
+            const response = await request('http://localhost:3000')
+                .post('/login') //o /login é a url
+                .set('Content-Type', 'application/json') //set é para colocar o header 
+                .send({ //send é o body
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                })
+
+            expect(response.status).to.equal(200);
+            expect(response.body.token).to.be.a('string');
+        })
+
+    })
+})
