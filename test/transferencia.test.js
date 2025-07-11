7eb28b41b1b8bf0eed31571eb6a640bb7ad56b1d
Michele <michelebrunasilva@gmail.com>
2025-07-11 14:17:18 -0300

    Adicionei testes para GET transferências total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -6,16 +6,16 @@ const postTransferencias = require('../fixtures/postTransferencias.json')
 
 
 describe('Transferências', () => {
-    describe('POST /transferencias', () => {
-        let token
+    let token
 
         beforeEach(async () =>{
-            // Capturar o token
             
+            // Capturar o token
             token = await obterToken('julio.lima', '123456')
 
         })
 
+    describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
             const bodyTransferencias = { ...postTransferencias} //shallow copy funciona apenas quando os dados estão em primeiro nível. Se houver uma lista, por exemplo, não funcionará
 
@@ -28,9 +28,8 @@ describe('Transferências', () => {
                 expect(response.status).to.equal(201)
 
                 console.log(response.body)
-
-
         })
+
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$ 10,00', async () => {
             const bodyTransferencias = { ...postTransferencias} 
@@ -43,8 +42,43 @@ describe('Transferências', () => {
                 .send(bodyTransferencias)
 
                 expect(response.status).to.equal(422)
+        })
+
+
+        describe('GET /transferencias/{id}', () =>{
+            it('Deve retornar sucesso com 200 e dados iguais ao registro de transferência contido no banco de dados quando o ID for válido', async () => {
+                const response = await request(process.env.BASE_URL)
+                    .get('/transferencias/15')
+                    .set('Authorization', `Bearer ${token}`)
+                
+            
+                expect(response.status).to.equal(200)
+                expect(response.body.id).to.equal(15)
+                expect(response.body.id).to.be.a('number')
+                expect(response.body.conta_origem_id).to.equal(1)
+                expect(response.body.conta_destino_id).to.equal(2)
+                expect(response.body.valor).to.equal(11.00)
+
+            })
 
         })
     })
+
+        describe('GET /transferencias', () => {
+            it ('Deve retornar 10 elementos na paginação', async () => {
+                const response = await request(process.env.BASE_URL)
+                    .get('/transferencias?page=1&limite=10')
+                    .set('Authorization', `Bearer ${token}`)
+
+                
+                expect(response.status).to.equal(200)
+                expect(response.body.limit).to.equal(10)
+                expect(response.body.transferencias).to.have.length(10)
+
+                console.log(response.body)
+
+            })
+
+        })
 
 })
